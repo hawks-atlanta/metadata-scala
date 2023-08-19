0eeb8f905919cf3bf4866a27dc8bc213957223b6
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-08-19 15:28:35 -0500

    feat(fruit): Sample files

---
+++ src/main/scala/fruit/domain/Fruit.scala
@@ -0,0 +1,4 @@
+package org.hawksatlanta.metadata
+package fruit.domain
+
+case class Fruit(id: String, name: String, price: Float)
---
+++ src/main/scala/fruit/infrastructure/InMemoryRepository.scala
@@ -0,0 +1,16 @@
+package org.hawksatlanta.metadata
+package fruit.infrastructure
+
+import fruit.domain.{Fruit, Repository}
+
+class InMemoryRepository extends Repository{
+  private var fruitsStore: List[Fruit] = List[Fruit]()
+
+  def get_fruits(): List[Fruit] = {
+    return fruitsStore
+  }
+
+  def save(fruit: Fruit): Unit = {
+    fruitsStore = fruitsStore :+ fruit
+  }
+}
