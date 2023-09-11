2ef830f54893a15f81b7756e2c580a2a49e9c36a
Pedro Andrés Chaparro Quintero <62714297+PedroChaparro@users.noreply.github.com>
2023-09-11 15:33:16 -0500

    refactor(tests): Global setup of HTTP server (#51)

--- src/test/scala/files_metadata/FilesTestsUtils.scala
+++ src/test/scala/files_metadata/FilesTestsUtils.scala
@@ -2,11 +2,22 @@ package org.hawksatlanta.metadata
 package files_metadata
 
 import java.util
+import java.util.concurrent.atomic.AtomicBoolean
 
 import io.restassured.response.Response
 import io.restassured.RestAssured.`given`
 
 object FilesTestsUtils {
+  var wasHttpServerInitializationCalled: AtomicBoolean = new AtomicBoolean(
+    false
+  )
+
+  def StartHttpServer(): Unit = {
+    if (wasHttpServerInitializationCalled.compareAndSet( false, true )) {
+      Main.main( Array[String]() )
+    }
+  }
+
   def SaveFile( payload: util.HashMap[String, Any] ): Response = {
     `given`()
       .port( 8080 )
