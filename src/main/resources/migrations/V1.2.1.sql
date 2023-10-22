14146fc7f94cde37a8a24b9062040a5d3f597d13
sdbaronc <47455237+sdbaronc@users.noreply.github.com>
2023-10-22 15:17:44 -0500

    refactor: repair migration statement

--- src/main/resources/migrations/V1.2.1.sql
+++ src/main/resources/migrations/V1.2.1.sql
@@ -4,6 +4,11 @@ ADD CONSTRAINT files_parent_uuid_fkey
 FOREIGN KEY (parent_uuid) REFERENCES files(uuid) ON DELETE CASCADE;
 
 ALTER TABLE files
-DROP CONSTRAINT IF EXISTS files_uuid_fkey,
-ADD CONSTRAINT files_uuid_fkey
-FOREIGN KEY (uuid) REFERENCES files(uuid) ON DELETE CASCADE;
+DROP CONSTRAINT IF EXISTS files_archive_uuid_fkey,
+ADD CONSTRAINT files_archive_uuid_fkey
+FOREIGN KEY (archive_uuid) REFERENCES archives(uuid) ON DELETE CASCADE;
+
+ALTER TABLE shared_files
+DROP CONSTRAINT IF EXISTS shared_files_file_uuid_fkey,
+ADD CONSTRAINT shared_files_file_uuid_fkey
+FOREIGN KEY (file_uuid) REFERENCES files(uuid) ON DELETE CASCADE;
